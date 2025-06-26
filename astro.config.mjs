8f683e4c86b952c19954d82887e0f2add680be87
teloong <lemindes@gmail.com>
2025-06-27 01:40:41 +0700

    Deploy: hanya hasil build dist

--- astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import astroIcon from 'astro-icon';
-/** @type {import('astro').AstroUserConfig} */
-export default defineConfig({
-  integrations: [tailwind(), astroIcon()]
-});
--- src/utils/supabase.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-export const supabaseUrl = 'https://jcfizceoycwdvpqpwhrj.supabase.co';
-export const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpjZml6Y2VveWN3ZHZwcXB3aHJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1NzUzNzUsImV4cCI6MjA2NDE1MTM3NX0.Au9FzSYvpaX7SkaVrgJvIgK9fZu5Dq4cU_NI5iwY6aA';
-export const supabase = createClient(supabaseUrl, supabaseKey);
